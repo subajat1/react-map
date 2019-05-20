83321cbfcf1c4d964ffbc48d1eaba929429da4d9
subajat1 <bayumunajat@outlook.com>
2019-05-20 15:06:55 +0800

    fix map loader

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 const mapStyles = {
@@ -6,16 +6,23 @@ const mapStyles = {
   height: '100%'
 };
 
+
 export class MapContainer extends Component {
+  
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+  };
+
   render() {
     return (
       <Map
         google={this.props.google}
-        zoom={14}
+        zoom={19}
         style={mapStyles}
         initialCenter={{
-         lat: -1.2884,
-         lng: 36.8233
+         lat: 1.299205,
+         lng: 103.786576
         }}
       />
     );
@@ -23,5 +30,5 @@ export class MapContainer extends Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyA9UTlOBbmDnTBTspvT-yhoQtS9wu-dogY'
-})(MapContainer);
+  apiKey: 'YOUR_GOOGLE_API_KEY_GOES_HERE'
+})(MapContainer)
